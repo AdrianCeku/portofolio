37c1117844bdec7b2e43d55c879f8fb046fe532f
AdrianCeku <cekuadrian4@gmail.com>
2023-05-25 05:40:05 +0200

    added animations

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const obeserver = new IntersectionObserver((elements) => {
+    elements.forEach((element) => {
+        console.log(element)
+        if (element.isIntersecting) {
+            element.target.classList.add("shown")
+        } 
+        else {
+            element.target.classList.remove("shown")
+        }
+    })
+})
+
+const hidden_elements = document.querySelectorAll(".hidden")
+hidden_elements.forEach((el) => obeserver.observe(el))

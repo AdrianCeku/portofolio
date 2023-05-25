bb515534bcf61c97e3d4760b1297a0d6d6e708a6
AdrianCeku <cekuadrian4@gmail.com>
2023-05-25 05:55:04 +0200

    improved animations and fixed bug where navbar link could be clicked despite not being extended

--- main.js
+++ main.js
@@ -18,3 +18,29 @@ const obeserver = new IntersectionObserver((elements) => {
 
 const hidden_elements = document.querySelectorAll(".hidden")
 hidden_elements.forEach((el) => obeserver.observe(el))
+
+
+/*
+let darkmode = true
+
+function switchMode() {
+    root = document.querySelector(":root")
+    if(darkmode==true) {
+        darkmode = false
+        root.style.setProperty("--bg-color-main", "#ffffff")
+        root.style.setProperty(" --bg-color-secondary", "#b8b8b8")
+        root.style.setProperty("--font-color-main", "#090000")
+        root.style.setProperty("--font-color-secondary", "#656565")
+        root.style.setProperty("--inverse-border-color", "#000000")
+    }
+    else {
+        darkmode= true
+        root.style.setProperty("--bg-color-main", "#0a000c")
+        root.style.setProperty(" --bg-color-secondary", "#090a10")
+        root.style.setProperty("--font-color-main", "#ffffff")
+        root.style.setProperty("--font-color-secondary", "#484848")
+        root.style.setProperty("--inverse-border-color", "#ffffff")
+    }
+}
+
+*/

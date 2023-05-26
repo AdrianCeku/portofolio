95d3e0c8cd532dcc55b252adf5f8752e742e4e2c
AdrianCeku <cekuadrian4@gmail.com>
2023-05-26 06:07:10 +0200

    added screen canvas to aracade
    added some svg for later use
    setup screen and textures so game.js can draw on the arcades screen in 3d.js

--- 3d.js
+++ 3d.js
@@ -1,60 +1,62 @@
 import "/style.css"
 import * as THREE from "three"
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 
 //Setup
 const loader = new GLTFLoader()
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(60, document.querySelector("#about").clientWidth / window.innerHeight, 0.1, 10000)
-camera.position.x = 319
-camera.position.y = 503
-camera.position.z = 23
-camera.rotation.x = -1.55
-camera.rotation.y = 1.16
-camera.rotation.z = 1.56
+
+const camera = new THREE.PerspectiveCamera(45, document.querySelector("#arcade").clientWidth / window.innerHeight, 0.1, 10000)
+camera.position.set(350, 400, 0)
+camera.rotation.set(degreeToRadian(0), degreeToRadian(90), degreeToRadian(0))
+
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#arcade')})
-const control = new OrbitControls(camera, renderer.domElement)
 
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(document.querySelector("#about").clientWidth, window.innerHeight)
 renderer.render(scene, camera)
 
-
+// Lighting
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5, 5, 5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
+// Screen
+const screenTexture = new THREE.CanvasTexture(document.querySelector("#game"));
+const screenGeometry = new THREE.PlaneGeometry(135, 170);
+const screenMaterial = new THREE.MeshBasicMaterial({ map: screenTexture });
+const screen = new THREE.Mesh(screenGeometry, screenMaterial);
+
+scene.add(screen);
+
+screen.position.set(20, 398, 0)
+screen.rotation.set(degreeToRadian(90), degreeToRadian(108), degreeToRadian(0))
+
 // Arcade
 let arcade
 loader.load("assets/models/arcade_game_machine_001/arcade.gltf", function (gltf) {
     arcade = gltf.scene
     scene.add(arcade)
+    arcade.castShadow = true
+    arcade.position.set(0,0,0)
+    arcade.rotation.set(degreeToRadian(0), degreeToRadian(0), degreeToRadian(0))
     console.log("Model loaded")
     animation()
 })
 
 
-// Create a texture for the arcade machine screen using the gameCanvas
-const screenTexture = new THREE.CanvasTexture(document.querySelector("#game"));
 
-// Create a plane geometry for the screen
-const screenGeometry = new THREE.PlaneGeometry(100, 100);
-const screenMaterial = new THREE.MeshBasicMaterial({ map: screenTexture });
-const screen = new THREE.Mesh(screenGeometry, screenMaterial);
 
-// Add the screen mesh to your scene
-scene.add(screen);
-console.log(screen.position[0])
+screen.attach(arcade)
 
 
 //responsive 3d 
 function update_size() {
-    camera.aspect = document.querySelector("#about").clientWidth / window.innerHeight
-    renderer.setSize(document.querySelector("#about").clientWidth, window.innerHeight)
+    camera.aspect = document.querySelector("#arcade").clientWidth / window.innerHeight
+    renderer.setSize(document.querySelector("#arcade").clientWidth, window.innerHeight)
 }
 window.onresize = update_size
 
@@ -63,17 +65,14 @@ let old_position
 
 function animation() {
     requestAnimationFrame(animation)
-    control.update()
     renderer.render(scene, camera)
 }
 
 // returns an inbetween value, between end and start value given the progress as a float between 0 and 1
+function degreeToRadian(degree) {
+    return degree * (Math.PI / 180)
+}
 
 function get_inbetween(start, end, progress){
     return (end * progress) + (start * (1-progress))
 }
-
-function keydown(key){
-}
-
-document.body.onkeydown = keydown
--- game.js
+++ game.js
@@ -3,4 +3,12 @@ import "/style.css"
 var canvas = document.querySelector("#game")
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+ctx.fillRect(0, 0, 75, 75);
+
+function keydown(key){
+    if(key["key"]=="e") {
+        console.log("e pressed")
+    }
+}
+
+document.body.onkeydown = keydown

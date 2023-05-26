3e3509e369c11723ff46d1e8248e30d8e4b6ad2b
AdrianCeku <cekuadrian4@gmail.com>
2023-05-26 06:34:52 +0200

    refactoring

--- 3d.js
+++ 3d.js
@@ -1,6 +1,8 @@
+
 import "/style.css"
 import * as THREE from "three"
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
+import { degToRad } from "three/src/math/MathUtils"
 
 
 //Setup
@@ -9,7 +11,7 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(45, document.querySelector("#arcade").clientWidth / window.innerHeight, 0.1, 10000)
 camera.position.set(350, 400, 0)
-camera.rotation.set(degreeToRadian(0), degreeToRadian(90), degreeToRadian(0))
+camera.rotation.set(degToRad(0), degToRad(90), degToRad(0))
 
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#arcade')})
 
@@ -33,7 +35,7 @@ const screen = new THREE.Mesh(screenGeometry, screenMaterial);
 scene.add(screen);
 
 screen.position.set(20, 398, 0)
-screen.rotation.set(degreeToRadian(90), degreeToRadian(108), degreeToRadian(0))
+screen.rotation.set(degToRad(90), degToRad(108), degToRad(0))
 
 // Arcade
 let arcade
@@ -42,16 +44,10 @@ loader.load("assets/models/arcade_game_machine_001/arcade.gltf", function (gltf)
     scene.add(arcade)
     arcade.castShadow = true
     arcade.position.set(0,0,0)
-    arcade.rotation.set(degreeToRadian(0), degreeToRadian(0), degreeToRadian(0))
+    arcade.rotation.set(degToRad(0), degToRad(0), degToRad(0))
     console.log("Model loaded")
     animation()
 })
-
-
-
-
-screen.attach(arcade)
-
 
 //responsive 3d 
 function update_size() {
@@ -60,19 +56,7 @@ function update_size() {
 }
 window.onresize = update_size
 
-let old_rotation
-let old_position
-
 function animation() {
     requestAnimationFrame(animation)
     renderer.render(scene, camera)
-}
-
-// returns an inbetween value, between end and start value given the progress as a float between 0 and 1
-function degreeToRadian(degree) {
-    return degree * (Math.PI / 180)
-}
-
-function get_inbetween(start, end, progress){
-    return (end * progress) + (start * (1-progress))
 }
--- main.js
+++ main.js
@@ -25,27 +25,7 @@ function current_scroll_progress()
     return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
 }
 
-/*
-let darkmode = true
-
-function switchMode() {
-    root = document.querySelector(":root")
-    if(darkmode==true) {
-        darkmode = false
-        root.style.setProperty("--bg-color-main", "#ffffff")
-        root.style.setProperty(" --bg-color-secondary", "#b8b8b8")
-        root.style.setProperty("--font-color-main", "#090000")
-        root.style.setProperty("--font-color-secondary", "#656565")
-        root.style.setProperty("--inverse-border-color", "#000000")
-    }
-    else {
-        darkmode= true
-        root.style.setProperty("--bg-color-main", "#0a000c")
-        root.style.setProperty(" --bg-color-secondary", "#090a10")
-        root.style.setProperty("--font-color-main", "#ffffff")
-        root.style.setProperty("--font-color-secondary", "#484848")
-        root.style.setProperty("--inverse-border-color", "#ffffff")
-    }
+// returns an inbetween value, between end and start value given the progress as a float between 0 and 1
+function getInbetween(start, end, progress){
+    return (end * progress) + (start * (1-progress))
 }
-
-*/
